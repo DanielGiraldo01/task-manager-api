3573a1870001fed402cc157140271b0fd41464ee
GLOBANT\daniel.giraldo01 <daniel.giraldo01>
2021-09-06 11:02:03 -0500

    Init commit

---
+++ src/routers/user.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const User = require('../models/user');
+const multer = require('multer');
+const sharp = require('sharp');
+const auth = require('../middleware/auth');
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account');
+// CREATE A NEW ROUTER
+const router = new express.Router();
+
+// POST USERS
+router.post('/users', async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const { email, name } = user;
+    await user.save();
+    sendWelcomeEmail(email, name);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+// LOGIN USER
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+// LOGOUT USER
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// LOGOUT ALL
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+// GET USERS
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
+});
+
+// GET USERS BY ID
+// router.get('/users/:id', async (req, res) => {
+//   const _id = req.params.id;
+
+//   try {
+//     const user = await User.findById(_id);
+//     if (!user) {
+//       return res.status(404).send();
+//     }
+//     res.send(user);
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// });
+
+// UPDATE USER BY ID
+router.patch('/users/me', auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(404).send(e);
+  }
+});
+
+router.delete('/users/me', auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    sendCancelationEmail(req.user.email, req.user.name);
+
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload a image'));
+    }
+    cb(undefined, true);
+  }
+});
+
+// ADD AVATAR
+router.post(
+  '/users/me/avatar',
+  auth,
+  upload.single('avatar'),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+// DELETE AVATAR
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+    res.set('Content-Type', 'image/png');
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
+module.exports = router;
